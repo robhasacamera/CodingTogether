7df494426b5069cb71d2277bca67e1b180114ab3
Robert Cole <robhasacamera@gmail.com>
2012-07-08 12:58:33 -0500

    Created private methods that determine whether a string is an operation and what type of operation it is. There will be used when implementing tickets #12, #13 and #14. Also reorganized the code into sections for easier readability.

--- Calculator/Calculator/CalculatorBrain.m
+++ Calculator/Calculator/CalculatorBrain.m
@@ -8,15 +8,37 @@
 
 #import "CalculatorBrain.h"
 
+#pragma mark - Private properties and operations
+
 @interface CalculatorBrain ()
 
 @property (nonatomic, strong) NSMutableArray *programStack;
+@property (nonatomic, strong) NSMutableDictionary *variableDictionary;
+
++ (BOOL)isOperation:(NSString *)operation;
+
++ (BOOL)isDoubleOperandOperation:(NSString *)operation;
+
++ (BOOL)isSingleOperandOperation:(NSString *)operation;
+
++ (BOOL)isNoOperandOperation:(NSString *)operation;
 
 @end
+
+#pragma mark - Implementation
 
 @implementation CalculatorBrain
 
 @synthesize programStack = _programStack;
+@synthesize variableDictionary = _variableDictionary;
+
+#pragma mark - Getters (public)
+
+- (id)program {
+    return [self.programStack copy];
+}
+
+#pragma mark - Getters (private)
 
 - (NSMutableArray *)programStack {
     if (!_programStack) {
@@ -26,8 +48,24 @@
     return _programStack;
 }
 
+- (NSMutableDictionary *)variableDictionary {
+    if (!_variableDictionary) {
+        _variableDictionary = [[NSMutableDictionary alloc]init];
+    }
+    
+    return _variableDictionary;
+}
+
+#pragma mark - Methods (public)
+
 - (void)pushOperand:(double)operand {
     [self.programStack addObject:[NSNumber numberWithDouble:operand]];
+}
+
+- (void)pushVariable:(NSString *)variable withValue:(double)value {
+    // check to see if its an operation
+    
+    //if its not an operation, add it to the variable dictionary
 }
 
 - (void)clearAllOperands {
@@ -39,14 +77,11 @@
     return [CalculatorBrain runProgram:self.program];
 }
 
-- (id)program {
-    return [self.programStack copy];
-}
-
-// TODO: Need to finish implementing this stub.
+// TODO: Need to implement according to ticket #14
 + (NSString *)descriptionOfProgram:(id)program {
     NSString *description = nil;
     
+    //this is not what's needed
     for (int i=0; i<[program count]; i++) {
         if (description) {
             description = [description stringByAppendingString:@" "];
@@ -56,6 +91,12 @@
         
         description = [description stringByAppendingFormat:@"%@", [program objectAtIndex:i]];
     }
+    
+    // regular (dual operand) operations are inserted before the last digit.
+    
+    // single operand operations 
+    
+    // pi and variables are inserted the same as doubles
     
     return description;
 }
@@ -110,6 +151,43 @@
     }
     
     return result;
+}
+
+#pragma mark - Methods (private)
+
++ (BOOL)isOperation:(NSString *)operation {
+    return [self isDoubleOperandOperation:operation] 
+    || [self isSingleOperandOperation:operation] 
+    || [self isNoOperandOperation:operation];
+}
+
++ (BOOL)isDoubleOperandOperation:(NSString *)operation {
+    NSSet *doubleOperandOperations = [[NSSet alloc]initWithObjects:
+                                      @"+",
+                                      @"*",
+                                      @"-"
+                                      @"/",
+                                      nil];
+    
+    return [doubleOperandOperations containsObject:operation];
+}
+
++ (BOOL)isSingleOperandOperation:(NSString *)operation {
+    NSSet *singleOperandOperations = [[NSSet alloc]initWithObjects:
+                                      @"sin",
+                                      @"cos",
+                                      @"sqrt",
+                                      nil];
+    
+    return [singleOperandOperations containsObject:operation];
+}
+
++ (BOOL)isNoOperandOperation:(NSString *)operation {
+    NSSet *noOperandOperations = [[NSSet alloc]initWithObjects:
+                                  @"pi",
+                                  nil];
+    
+    return [noOperandOperations containsObject:operation];
 }
 
 @end
