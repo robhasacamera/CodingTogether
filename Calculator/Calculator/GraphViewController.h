d957d6262fe40ca41bb56968c83f1b59c2076f85
Robert Cole <robhasacamera@gmail.com>
2012-07-13 23:57:55 -0500

    Added new class GraphViewController and made the new view controller on the storyboard a member of this class. This is for ticket #22.

---
+++ Calculator/Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by Robert Cole on 7/13/12.
+//  Copyright (c) 2012 Clearwire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
---
+++ Calculator/Calculator/GraphViewController.m
@@ -0,0 +1,43 @@
+//
+//  GraphViewController.m
+//  Calculator
+//
+//  Created by Robert Cole on 7/13/12.
+//  Copyright (c) 2012 Clearwire. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
